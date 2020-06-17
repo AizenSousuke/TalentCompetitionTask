f4eea6e6e86b9c4324b4086c3011709396d93e35
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-06-17 21:41:03 +0800

    Added GET

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -1,84 +1,114 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-import Cookies from 'js-cookie';
-import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
-import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
-import { JobSummaryCard } from './JobSummaryCard.jsx';
-import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
-import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import Cookies from "js-cookie";
+import LoggedInBanner from "../../Layout/Banner/LoggedInBanner.jsx";
+import { LoggedInNavigation } from "../../Layout/LoggedInNavigation.jsx";
+import { JobSummaryCard } from "./JobSummaryCard.jsx";
+import { BodyWrapper, loaderData } from "../../Layout/BodyWrapper.jsx";
+import {
+	Pagination,
+	Icon,
+	Dropdown,
+	Checkbox,
+	Accordion,
+	Form,
+	Segment,
+} from "semantic-ui-react";
 
 export default class ManageJob extends React.Component {
-    constructor(props) {
-        super(props);
-        let loader = loaderData
-        loader.allowedUsers.push("Employer");
-        loader.allowedUsers.push("Recruiter");
-        //console.log(loader)
-        this.state = {
-            loadJobs: [],
-            loaderData: loader,
-            activePage: 1,
-            sortBy: {
-                date: "desc"
-            },
-            filter: {
-                showActive: true,
-                showClosed: false,
-                showDraft: true,
-                showExpired: true,
-                showUnexpired: true
-            },
-            totalPages: 1,
-            activeIndex: ""
-        }
-        this.loadData = this.loadData.bind(this);
-        this.init = this.init.bind(this);
-        this.loadNewData = this.loadNewData.bind(this);
-        //your functions go here
-    };
+	constructor(props) {
+		super(props);
+		let loader = loaderData;
+		loader.allowedUsers.push("Employer");
+		loader.allowedUsers.push("Recruiter");
+		//console.log(loader)
+		this.state = {
+			loadJobs: [],
+			loaderData: loader,
+			activePage: 1,
+			sortBy: {
+				date: "desc",
+			},
+			filter: {
+				showActive: true,
+				showClosed: false,
+				showDraft: true,
+				showExpired: true,
+				showUnexpired: true,
+			},
+			totalPages: 1,
+			activeIndex: "",
+		};
+		this.loadData = this.loadData.bind(this);
+		this.init = this.init.bind(this);
+		this.loadNewData = this.loadNewData.bind(this);
+		//your functions go here
+	}
 
-    init() {
-        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
-        loaderData.isLoading = false;
-        this.setState({ loaderData });//comment this
+	init() {
+		console.log("Init\\Refresh page", this.state);
+		let loaderData = TalentUtil.deepCopy(this.state.loaderData);
+		loaderData.isLoading = true;
 
-        //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
-        
-        //console.log(this.state.loaderData)
-    }
+		//set loaderData.isLoading to false after getting data
+		this.loadData((data) => {
+            console.log("Data loaded: ", data);
+            loaderData.isLoading = false;
+			this.setState({ loaderData }, () => {
+				// console.log("Loader Data: ", this.state.loaderData);
+			});
+		});
+	}
 
-    componentDidMount() {
-        this.init();
-    };
+	componentDidMount() {
+		this.init();
+	}
 
-    loadData(callback) {
-        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
-        var cookies = Cookies.get('talentAuthToken');
-       // your ajax call and other logic goes here
-    }
+	loadData(callback) {
+        console.log("Calling loadData");
+		var link =
+			"http://localhost:51689/listing/listing/getSortedEmployerJobs";
+		var cookies = Cookies.get("talentAuthToken");
+		// your ajax call and other logic goes here
+		var header = {
+			Authorization: "Bearer " + cookies,
+			"Content-Type": "application/json; charset=utf-8",
+        };
 
-    loadNewData(data) {
-        var loader = this.state.loaderData;
-        loader.isLoading = true;
-        data[loaderData] = loader;
-        this.setState(data, () => {
-            this.loadData(() => {
-                loader.isLoading = false;
-                this.setState({
-                    loadData: loader
-                })
-            })
+		fetch(link, {
+			method: "GET",
+			headers: header,
+		}).then((data) => {
+            return data.json();
+		}).then((data) => {
+            console.log("GET RESPONSE: ", data);
+            callback(data);
         });
-    }
+	}
 
-    render() {
-        return (
-            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
-            </BodyWrapper>
-        )
-    }
+	loadNewData(data) {
+		console.log("Loading new data", data);
+		var loader = this.state.loaderData;
+		loader.isLoading = true;
+		data[loaderData] = loader;
+		this.setState(data, () => {
+			this.loadData(() => {
+				loader.isLoading = false;
+				this.setState({
+					loadData: loader,
+				});
+			});
+		});
+	}
+
+	render() {
+		return (
+			<BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+				<div className="ui container">
+					Your table goes here
+					<h1>List of Jobs</h1>
+				</div>
+			</BodyWrapper>
+		);
+	}
 }
